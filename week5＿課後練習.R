ad43e31beffbbadcb0805918562f13dcf3c7a3b6
Yijun <yeyijun@X.local>
2024-03-21 14:48:18 +0800

    week5_v1

---
+++ week5＿課後練習.R
@@ -0,0 +1,117 @@
+library(tidyverse)
+
+# 創建一個空的列表，用於存儲所有學年的 CSV 下載網址
+csv_urls <- list()
+
+# 迴圈生成所有學年的 CSV 下載網址
+for (year in 104:112) {
+  # 生成學年的 CSV 下載網址並添加到列表中
+  csv_urls[[as.character(year)]] <- paste0("https://stats.moe.gov.tw/files/ebook/native/", year, "/", year, "native_A1-1.csv")
+}
+
+# 將結果存入results中
+results <- list(csv_urls = csv_urls)
+print(csv_urls)
+
+## download and import ----
+# 建立一個空的list來存儲下載的資料
+data_list <- list()
+
+# 遍歷所有學年的 CSV 下載網址
+for (year in names(csv_urls)) {
+  # 排除110年，单独处理
+  if (year == 110) {
+    # 下载 CSV 文件，并指定文件编码为BIG5
+    download.file(csv_urls[[year]], destfile = paste0(year, ".csv"))
+    
+    # 读取 CSV 文件并存储到列表中
+    data_list[[year]] <- read.csv(paste0(year, ".csv"), fileEncoding = "BIG5")
+  } else {
+    # 下载并读取其他学年的 CSV 文件
+    download.file(csv_urls[[year]], destfile = paste0(year, ".csv"))
+    data_list[[year]] <- read.csv(paste0(year, ".csv"))
+  }
+}
+
+'''
+for (year in names(csv_urls)) {
+  # 下載 CSV 文件
+  download.file(csv_urls[[year]], destfile = paste0(year, ".csv"))
+  
+  # 讀取 CSV 文件並存儲到列表中
+  data_list[[year]] <- read.csv(paste0(year, ".csv"))
+}
+
+# 將結果存儲在環境中已有的list物件results中
+results$data_list <- data_list
+'''
+
+library(purrr)
+library(dplyr)
+
+# 使用imap()函數向每個DataFrame添加"學年度"欄位
+data_list <- imap(data_list, ~ mutate(.x, 學年度 = as.integer(gsub("\\D", "", .y))))
+
+# 將結果存儲在環境中已有的list物件results中
+results$data_list_with_year <- data_list
+
+# merge ----
+# 垂直合併data_list中的每個DataFrame
+combined_data <- bind_rows(data_list)
+
+# 將結果存儲在環境中已有的list物件results中
+results$combined_data <- combined_data
+
+view(combined_data)
+
+## 畫圖
+library(ggplot2)
+
+# 计算每个学年各学制的人数总和
+sum_data <- combined_data %>%
+  group_by(學年度) %>%
+  summarise(
+    在學學生人數_博士班 = sum(在學學生人數_博士班),
+    在學學生人數_碩士班 = sum(在學學生人數_碩士班),
+    在學學生人數_學士班 = sum(在學學生人數_學士班)
+  ) %>%
+  pivot_longer(cols = starts_with("在學學生人數_"), names_to = "學制", values_to = "在學學生人數")
+
+# 定義學制levels
+# 将学年度列转换为因子变量，并指定自定义的级别
+sum_data$學制 <- factor(sum_data$學制, levels = c("在學學生人數_博士班", "在學學生人數_碩士班", "在學學生人數_學士班"))
+
+'''
+# 绘制堆积条形图
+ggplot(sum_data, aes(x = 在學學生人數, y = factor(學年度), fill = 學制)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "各學年學制人數佔比",
+    x = "在學學生人數",
+    y = "學年度",
+    fill = "學制"
+  ) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+'''
+
+# 绘制 ggplot2 图表
+library(plotly)
+
+gg <- ggplot(sum_data, aes(x = 在學學生人數, y = factor(學年度), fill = 學制)) +
+  geom_bar(stat = "identity") +
+  labs(
+    title = "各學年學制人數佔比",
+    x = "在學學生人數",
+    y = "學年度",
+    fill = "學制"
+  ) +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# 将 ggplot2 图表转换为 Plotly 图表
+plotly_chart <- ggplotly(gg)
+
+# 显示 Plotly 图表
+plotly_chart
+
