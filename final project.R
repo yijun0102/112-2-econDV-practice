9e4ddf14cf92d3e8835159c3b559221deba38677
Yijun <yeyijun@X.local>
2024-05-28 00:48:28 +0800

    v5

--- final project.R
+++ final project.R
@@ -4,16 +4,16 @@ library(ggplot2)
 library(plotly)
 
 # 讀取資料
-house <- read_csv("/Users/yeyijun/Documents/EconDV/112-2-econDV-practice/房價所得比2002-2022.csv")
+house_Q <- read_csv("/Users/yeyijun/Documents/EconDV/112-2-econDV-practice/房價所得比2002-2022.csv")
 birth <- read_csv("/Users/yeyijun/Documents/EconDV/112-2-econDV-practice/生育率2002-2022.csv")
 
 # 移除空值欄位
-house[house == "---"] <- NA
-house <- house %>%
+house_Q[house_Q == "---"] <- NA
+house_Q <- house_Q %>%
   select_if(function(col) !all(is.na(col)))
 
 # 計算每四筆資料加總
-house <- house %>%
+house <- house_Q %>%
   mutate(年度季別 = as.numeric(substring(年度季別, 1, 3)) + 1911) %>%
   group_by(年度季別) %>%
   summarise_all(mean, na.rm = TRUE)
@@ -144,3 +144,62 @@ plot <- plot %>%
 plot
 
 
+#############################plt2
+# 繪製房價-時間關係圖
+house_Q$year[house_Q$年度季別] <- "substr(年度季別, 1, 3) + 1911)"
+
+# 繪製房價-時間關係圖
+plot2 <- plot_ly() %>%
+  add_trace(
+    data = house_Q %>% select(-全國, -台北市) %>% pivot_longer(-c(年度季別, year), names_to = "city", values_to = "value"),
+    x = ~年度季別, y = ~value, type = 'scatter', mode = 'lines',
+    line = list(color = "rgba(194, 193, 190, 0.3)", width = 5),
+    name = ~city,
+    showlegend = FALSE
+  ) %>%
+  add_trace(
+    data = house_Q,
+    x = ~年度季別, y = ~台北市, type = 'scatter', mode = 'lines',
+    line = list(color = "#E76F51", width = 5),
+    name = "台北市"
+  ) %>%
+  add_trace(
+    data = house_Q,
+    x = ~年度季別, y = ~全國, type = 'scatter', mode = 'lines',
+    line = list(color = "#2A9D8f", width = 5),
+    name = "全國"
+  ) %>%
+  layout(
+    xaxis = list(
+      title = "年度季別",
+      showline = TRUE,
+      zeroline = TRUE,
+      tickangle = 45
+    ),
+    yaxis = list(title = "房價所得比", showline = TRUE, zeroline = TRUE, automargin = TRUE),
+    showlegend = TRUE,
+    title = list(
+      text = "<b>房價趨勢圖</b><br><span style='font-size:15px;'>台灣近20年</span>",
+      x = 0.01,
+      y = 0.97,
+      xanchor = 'left',
+      yanchor = 'top',
+      font = list(size = 20)
+    ),
+    margin = list(t = 100, b = 100),
+    annotations = list(
+      list(
+        x = -0.06,
+        y = -0.15,
+        xref = "paper",
+        yref = "paper",
+        text = "資料來源：內政部不動產資訊平台",
+        showarrow = FALSE,
+        xanchor = 'left',
+        yanchor = 'bottom'
+      )
+    )
+  )
+
+# 顯示圖表
+plot2
